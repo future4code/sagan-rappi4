48b3af36084d36a551f78cf383a0b32434228a83
Nauara Melo <nauaramelo@gmail.com>
2020-04-15 09:48:34 -0300

    integrando reducers de restaurantes

--- src/reducers/restaurants.js
+++ src/reducers/restaurants.js
@@ -1,13 +1,33 @@
 const initialState = {
     restaurantsList: [],
+    restaurantDetailed: {},
+    cart: {},
   }
   
   const restaurants = (state = initialState, action) => {
     switch (action.type) {
-      case 'GET_RESTAURANTS':
+      case 'GET_RESTAURANTS': {
         return {
-          ...state, restaurantsList: action.payload.restaurants
+          ...state, 
+          restaurantsList: action.payload.restaurants
         }
+      }
+
+      case 'SET_RESTAURANT': {
+        return {
+          ...state,
+          restaurantDetailed: action.payload.restaurant
+        }
+      }
+
+      case 'SET_CART': {
+        console.log(action.payload.cart)
+        return {
+          ...state,
+          cart: action.payload.cart
+        }
+      }
+
       default:
         return state
     }
