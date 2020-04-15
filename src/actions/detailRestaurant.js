5ee6822ad065d65ccfc68caabe26282f0bec9769
Nauara Melo <nauaramelo@gmail.com>
2020-04-15 09:52:28 -0300

    modificando token

--- src/actions/detailRestaurant.js
+++ src/actions/detailRestaurant.js
@@ -22,8 +22,7 @@ export const setCart = (cart) => {
 
 
 export const getRestaurantDetail = (id) => async (dispatch) => {
-    /* const token = window.localStorage.getItem('token') */
-    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImdhVmRYd1RSSms2MWJyMThHMHBtIiwibmFtZSI6IkZ1bGFubyIsImVtYWlsIjoiZnVsYW5vQGdtYWlsLmNvbSIsImNwZiI6IjExMS4yMjIuMzMzLTExIiwiaGFzQWRkcmVzcyI6dHJ1ZSwiYWRkcmVzcyI6IlIuIEFmb25zbyBCcmF6LCAxNzcsIDcxIC0gVmlsYSBOLiBDb25jZWnDp8OjbyIsImlhdCI6MTU4Njc5NzY3NX0.4ztoLx6x3H5Ex0R4a5Msh2OvGEfEqR2RTTziRTWib3k"
+    const token = window.localStorage.getItem('token')
     
     try {
         const response = await axios.get(
